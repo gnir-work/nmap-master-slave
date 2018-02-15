043a695e1a5b866758932dbbf8cf94dca9545f1b
nir geller <geller.nir@gmail.com>
2018-02-15 15:01:23 +0200

    Added the ability to run several slaves

--- src/nmap-master-slave/master.py
+++ src/nmap-master-slave/master.py
@@ -2,29 +2,34 @@ import zmq
 import sys
 from logbook import StreamHandler, Logger, FileHandler
 from consts import MASTER_LOG_FILE, REPORT_PORT, MASTER_IP
+from itertools import cycle
 
 # StreamHandler(sys.stdout, bubble=True, level='DEBUG').push_application()
 FileHandler(MASTER_LOG_FILE, bubble=True, level='INFO').push_application()
 logger = Logger('Master')
 context = zmq.Context()
+SLAVE_PORTS = [5555, 5556]
 
 
 def _retrieve_ips_to_scan():
-    return ['127.0.0.1']
+    return ['127.0.0.1', '127.0.0.1', '127.0.0.1', '127.0.0.1', '127.0.0.1', '127.0.0.1']
+
+
+def _create_slave_socket(port):
+    slave_socket = context.socket(zmq.PUSH)
+    slave_socket.bind('tcp://{ip}:{port}'.format(ip=MASTER_IP, port=port))
 
 
 def start_master():
-    # Slave socket
-    slave_socket = context.socket(zmq.PUSH)
-    slave_socket.bind('tcp://{ip}:5555'.format(ip=MASTER_IP))
+    slave_sockets_iter = cycle(map(_create_slave_socket, SLAVE_PORTS))
     ips_to_scan = _retrieve_ips_to_scan()
     # receive status
     reporter = context.socket(zmq.PULL)
     reporter.bind("tcp://{ip}:{port}".format(ip=MASTER_IP, port=REPORT_PORT))
 
-    for ip in ips_to_scan:
+    for index, ip in enumerate(ips_to_scan):
         print('sending', ip)
-        slave_socket.send_json({'ip': ip, 'ports': '1-10', 'opt': 'sS'})
+        next(slave_sockets_iter).send_json({'ip': ip, 'ports': '1-10', 'opt': 'sS'})
 
     for ip in ips_to_scan:
         print('done', ip, reporter.recv_json())
