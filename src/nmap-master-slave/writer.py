7e750614df502244a816c7fdb3b871f347853ec4
nir geller <geller.nir@gmail.com>
2018-02-15 18:37:06 +0200

    Fixed check for closed ports

--- src/nmap-master-slave/writer.py
+++ src/nmap-master-slave/writer.py
@@ -31,8 +31,7 @@ class MysqlWriter(object):
         for protocol in filter(lambda prot: prot in PROTOCOLS, result['scan'][host]):
             for port in result['scan'][host][protocol]:
                 try:
-                    if not self.ignore_closed_ports or result['scan'][host][protocol][port]['reason'] in (
-                            'no-response', 'reset'):
+                    if not (self.ignore_closed_ports and result['scan'][host][protocol][port]['state'] == 'closed'):
                         scanned_port = PortScan(port=port,
                                                 protocol=protocol,
                                                 state=result['scan'][host][protocol][port]['state'],
